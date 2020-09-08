e3992c691d61c580e7ea5b4f55bcf8accb772f35
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-09-08 10:24:20 -1000

    override theme logo with Kodi logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,17 +35,18 @@ const sideNav = [
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Kodi TV',
+    description: 'The web site for the Kodi media center.',
+    author: 'Team Kodi',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-root-import',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'images',
+        path: 'src/images',
       },
     },
     {
@@ -61,22 +62,10 @@ module.exports = {
         sideNav: sideNav
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
 }
---
+++ src/gatsby-theme-patternfly/components/Logo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Brand } from '@patternfly/react-core';
+import brandImg from 'src/images/kodi-logo.png';
+
+const Logo = () => <Brand src={brandImg} alt="Kodi"/>;
+
+export default Logo;
