14c932720cdce833f94e6a3416599e866659e79d
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-09-15 12:44:45 -1000

    fix for a slew of build warnings (that can technically be ignored)

--- gatsby-node.js
+++ gatsby-node.js
@@ -104,3 +104,17 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+// this fixes a slew of build warnings
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(
+      plugin => plugin.constructor.name === 'MiniCssExtractPlugin'
+    )
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+}
