20c64f28888ecf6883b85291272c49e812154118
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-02-27 18:29:24 -1000

    enabling gatsby conditional page builds

--- gatsby-config.js
+++ gatsby-config.js
@@ -296,6 +296,14 @@ module.exports = {
     },
     "gatsby-source-kodidists",
     "gatsby-source-kodistore",
+    {
+    resolve: "gatsby-plugin-netlify-cache",
+    options: {
+      extraDirsToCache: [
+        "src/data"
+      ]
+    }
+  }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
