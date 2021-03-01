edfd355016940fbc7f89cd5d00f27027141e0546
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-03-01 10:04:38 -1000

    update to (hopefully) fix build issue on Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -182,7 +182,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `src/images/icon.png`,
+        icon: `src/images/icon.jpg`,
       },
     },
     {
