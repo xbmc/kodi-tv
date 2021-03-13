839f01b7829c53fc28331f853e88a4bcf7272323
Darren-Hill <darrenhill@kodi.tv>
2021-03-13 16:50:09 +0000

    Minor page updates
    Changes the site title text from Opensource to Open Source (just for Keith :) ) and some adjustments to the about index page.

--- gatsby-site-config.js
+++ gatsby-site-config.js
@@ -8,7 +8,7 @@ if (process.env.GATSBY_SITEURL === undefined) {
 
 module.exports = {
   siteMetadata: {
-    title: "Kodi | Opensource Home Theater Software",
+    title: "Kodi | Open Source Home Theater Software",
     description: "Kodi is a free media player that is designed to look great on your big screen TV but is just as home on a small screen.",
     siteUrl: url,
     author: "Team Kodi",
