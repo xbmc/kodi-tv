4cc12d3132cf181f2b5b0221b9813a352d76e275
Razze <razzeee@gmail.com>
2021-03-14 03:13:58 +0100

    Reimplement a few components

---
+++ src/components/ArrowRightIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ArrowRight } from "heroicons-react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ArrowRight className="inline" />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Button;
---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface Props {
+  href: string;
+}
+
+class Button extends React.Component<Props> {
+  render() {
+    return (
+      <a href={this.props.href}>
+        <button
+          type="button"
+          className="border border-kodi bg-kodi text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-kodi-darker focus:outline-none focus:shadow-outline"
+        >
+          <span>{this.props.children}</span>
+        </button>
+      </a>
+    );
+  }
+}
+
+export default Button;
