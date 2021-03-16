31390e95d27e8a7fed7a9b9d86d1a92edea37209
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-03-15 21:27:41 -1000

    reformat

--- src/components/IconList.tsx
+++ src/components/IconList.tsx
@@ -42,7 +42,10 @@ export default function categoryIconList(props: {
         <div align="center">
           <Link className="underline text-blue-500" to={linkroot + item.slug}>
             <img width={iconwidth} height={iconheight} alt="" src={item.icon} />
-            <div style={{ display: item.name === '' ? "none" : "block" }} dangerouslySetInnerHTML={{ __html: item.name }} />
+            <div
+              style={{ display: item.name === "" ? "none" : "block" }}
+              dangerouslySetInnerHTML={{ __html: item.name }}
+            />
           </Link>
         </div>
       ))}
