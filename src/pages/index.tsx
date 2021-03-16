249ab91d06585b91e2d1a8fa4827c23f1d05d66b
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-03-15 14:05:48 -1000

    more updates to the new index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import Hero from "src/components/hero";
+import { HeroFullWidth } from "src/components/hero";
+import IconList from "src/components/IconList"
+import * as Icons from "heroicons-react";
 import { LandingPage } from "/src/components/layout";
+
+let frontmatter = {title: "", breadcrumbs: "Home"}
 
 class Page extends React.Component {
   render() {
     return (
-      <LandingPage>
-        <Hero />
-        <div className=" pt-6 pb-12">
-          <div className="text-center text-7xl font-bold">
+      <LandingPage frontmatter={frontmatter}>
+
+        <HeroFullWidth />
+
+        <div className=" pt-12 pb-20 px-6">
+          <div className="text-center text-5xl font-bold">
             Kodi does it all
           </div>
           <div className="text-center text-2xl text-gray-400">
@@ -16,115 +22,222 @@ class Page extends React.Component {
           </div>
         </div>
 
-    <div className="bg-gradient-to-r from-kodi-lighter to-kodi-darker">
-      <div className="max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8">
-        <h2 className="text-3xl font-extrabold text-white tracking-tight">
-          Kodi is the ultimate entertainment center software
-        </h2>
-        <p className="mt-4 max-w-3xl text-lg text-gray-200">
-          It plays almost every kind of media you can find and it looks great while doing it! Here are just a few things Kodi excels at:
-        </p>
-        <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
-          <div>
-            <div>
-              <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
-                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
-                </svg>
-              </span>
-            </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-medium text-white">Music</h3>
-              <p className="mt-2 text-base text-gray-200">
-                Your music collection has never looked this good! Support for almost every format, playlists, party mixes and more.
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div>
-              <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
-                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-                </svg>
-              </span>
-            </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-medium text-white">Movies</h3>
-              <p className="mt-2 text-base text-gray-200">
-                So much better than a pile of DVDs on a shelf. Kodi brings your movie collection to life with artwork, cast genres and more.
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div>
-              <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
-                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                </svg>
-              </span>
-            </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-medium text-white">TV Shows</h3>
-              <p className="mt-2 text-base text-gray-200">
-                Perfect for binge watching or the occasional catch up on your favourite show. Kodi keeps all your tv organised like nothing else.
-              </p>
+        <div className="relative pt-16 pb-32 overflow-hidden">
+          <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100"></div>
+          <div className="relative">
+            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+              <div className="px-6 max-w-xl mx-auto lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+                <div>
+                  <div>
+                    <span className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-kodi-darker to-kodi-lighter">
+                      <Icons.DownloadOutline className="h-6 w-6 text-white" />
+                    </span>
+                  </div>
+                  <div className="mt-6">
+                    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                      Kodi runs on just about anything
+                    </h2>
+                    <p className="mt-4 text-lg text-gray-500">
+                      Kodi runs on a huge range of devices and operating systems, including:
+                      <ul className="pl-10 pt-4">
+                        <li>Windows</li>
+                        <li>macOS, iOS, and tvOS</li>
+                        <li>Android</li>
+                        <li>Linux</li>
+                        <li>Raspberry Pi</li>
+                      </ul>
+                    </p>
+                    <div className="mt-6">
+                      <a href="/download" className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-kodi-lighter to-kodi-darker">
+                        Download Now
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden lg:block mt-16 lg:mt-0">
+                <div className="-mr-48 pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                  <IconList
+                    items={
+                      [
+                        {slug: "", icon: "/images/distributions/windows.png", name: ""},
+                        {slug: "", icon: "/images/distributions/macos.png", name: ""},
+                        {slug: "", icon: "/images/distributions/android.png", name: ""},
+                        {slug: "", icon: "/images/distributions/linux.png", name: ""},
+                        {slug: "", icon: "/images/distributions/raspberry-pi.png", name: ""},
+                      ]
+                    }
+                    linkroot="/download"
+                    iconwidth="150"
+                    className="grid grid-cols-2 lg:pt-6 gap-8"
+                  />
+                </div>
+              </div>
             </div>
           </div>
+        </div>
 
-          <div>
-            <div>
-              <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
-                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                </svg>
-              </span>
-            </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-medium text-white">Photos</h3>
-              <p className="mt-2 text-base text-gray-200">
-                Kodi is the best way to share your photos on the biggest screen in the house with a personal slideshow.
-              </p>
+        <div className="bg-gradient-to-r from-kodi-lighter to-kodi-darker">
+          <div className="max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8">
+            <h2 className="text-3xl font-extrabold text-white tracking-tight">
+              Kodi is the ultimate entertainment center software
+            </h2>
+            <p className="mt-4 max-w-3xl text-lg text-gray-200">
+              It plays almost every kind of media you can find and it looks great while doing it! Here are just a few things Kodi excels at:
+            </p>
+            <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+              <div>
+                <div>
+                  <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
+                    <Icons.MusicNoteOutline className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-white">Music</h3>
+                  <p className="mt-2 text-base text-gray-200">
+                    Your music collection has never looked this good! Support for almost every format, playlists, party mixes and more.
+                  </p>
+                </div>
+              </div>
+
+              <div>
+                <div>
+                  <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
+                    <Icons.FilmOutline className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-white">Movies</h3>
+                  <p className="mt-2 text-base text-gray-200">
+                    So much better than a pile of DVDs on a shelf. Kodi brings your movie collection to life with artwork, cast genres and more.
+                  </p>
+                </div>
+              </div>
+    
+              <div>
+                <div>
+                  <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
+                    <Icons.VideoCameraOutline className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-white">TV Shows</h3>
+                  <p className="mt-2 text-base text-gray-200">
+                    Perfect for binge watching or the occasional catch up on your favourite show. Kodi keeps all your tv organised like nothing else.
+                  </p>
+                </div>
+              </div>
+    
+              <div>
+                <div>
+                  <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
+                    <Icons.PhotographOutline className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-white">Photos</h3>
+                  <p className="mt-2 text-base text-gray-200">
+                    Kodi is the best way to share your photos on the biggest screen in the house with a personal slideshow.
+                  </p>
+                </div>
+              </div>
+    
+              <div>
+                <div>
+                  <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
+                    <Icons.ClockOutline className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-white">PVR</h3>
+                  <p className="mt-2 text-base text-gray-200">
+                    Kodi allows you to watch and record live TV all from an easy to use interface. It works with a number of popular backends including MediaPortal, MythTV, NextPVR, Tvheadend and many more.
+                  </p>
+                </div>
+              </div>
+    
+              <div>
+                <div>
+                  <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
+                    <Icons.PuzzleOutline className="h-6 w-6 text-white" />
+                  </span>
+                </div>
+                <div className="mt-6">
+                  <h3 className="text-lg font-medium text-white">Games</h3>
+                  <p className="mt-2 text-base text-gray-200">
+                    Kodi isn"t just for passive entertainment media. You can also play games on Kodi.  Pick from a large number of emulators, or even play a stand-alone game.  There"s even extensive support for game controllers.
+                  </p>
+                </div>
+              </div>
+    
             </div>
           </div>
-
-          <div>
+        </div>
+    
+        <div className="relative pt-16 pb-32 overflow-hidden">
+          <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100"></div>
+      <div className="mt-24">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
             <div>
-              <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
-                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-                </svg>
-              </span>
-            </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-medium text-white">PVR</h3>
-              <p className="mt-2 text-base text-gray-200">
-                Kodi allows you to watch and record live TV all from easy to use interface. It works with a number of popular backends including MediaPortal, MythTV, NextPVR, Tvheadend and many more.
-              </p>
+              <div>
+                <span className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-kodi-darker to-kodi-lighter">
+                  <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+                  </svg>
+                </span>
+              </div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  But wait, there's more!
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                  You can get a new skin to change the look of Kodi, or maybe you want to stream your favourite internet radio station and control it all with your web browser or mobile phone? This and so much more is all possible and easy with Kodi.
+                </p>
+                <div className="mt-6">
+                  <a href="/about" className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-kodi-lighter to-kodi-darker">
+                    Show me all the good bits
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
-
-          <div>
-            <div>
-              <span className="flex items-center justify-center h-12 w-12 rounded-md bg-white bg-opacity-10">
-                <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 10h10a8 8 0 018 8v2M3 10l6 6m-6-6l6-6" />
-                </svg>
-              </span>
-            </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-medium text-white">Games</h3>
-              <p className="mt-2 text-base text-gray-200">
-                Kodi isn't just for passive entertainment media. You can also play games on Kodi.  Pick from a large number of emulators, or even play a stand-alone game.  There's even extensive support for game controllers.
-              </p>
+          <div className="hidden lg:block mt-16 lg:mt-0 lg:col-start-1">
+            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <img className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="/images/skin-arctic-horizon-example.png" alt="Artic Horizon skin" />
             </div>
           </div>
+        </div>
+      </div>
+        </div>
 
+<div className="relative bg-gray-800">
+  <div className="h-56 md:absolute md:left-0 md:h-full md:w-1/2">
+    <img className="w-full h-full object-cover" src="/images/people-working.png" alt="three people working at a computer" />
+    <div
+      className="absolute inset-0 bg-kodi-darker"
+      style={{ mixBlendMode: "multiply" }}
+    ></div>
+
+  </div>
+  <div className="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+    <div className="md:ml-auto md:w-1/2 md:pl-10">
+      <h2 className="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">
+        We need your help
+      </h2>
+      <p className="mt-3 text-lg text-gray-300">
+        You can help in so many different ways, as you may or may not know, Kodi is maintained completely by volunteers and we need and value any contributions to the project. If you can code, we are always on the lookout for new team members to assist with development. If you are good with your words then you can help others in the forum, with QA or translations. If you are time poor but still want to give back, then you can donate or purchase some great Kodi merch. Visit our contribute page for all the other ways you can help.
+      </p>
+      <div className="mt-8">
+        <div className="inline-flex rounded-md shadow">
+          <a href="/contribute" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50">
+            Find Ways to Help
+          </a>
         </div>
       </div>
     </div>
-
+  </div>
+</div>
 
         <div className="bg-gray-100">
           <div className="max-w-7xl mx-auto py-16 px-4 px-6 lg:px-8">
