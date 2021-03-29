8c15f500591a4c702e5e64bb333efc8d0b7ad783
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-03-28 17:04:50 -1000

    fix for CMS not linking to correct branch

--- gatsby-site-config.js
+++ gatsby-site-config.js
@@ -5,17 +5,17 @@ if (process.env.GATSBY_SITEURL === undefined) {
 } else {
   url = process.env.GATSBY_SITEURL;
 }
-if (process.env.NETLIFYCMS_BACKEND_BRANCH === undefined) {
+if (process.env.NETLIFY_CMS_BACKEND === undefined) {
   backend_branch = "staging";
 } else {
-  backend_branch = process.env.NETLIFYCMS_BACKEND_BRANCH;
+  backend_branch = process.env.NETLIFY_CMS_BACKEND;
 }
 
 module.exports = {
   siteMetadata: {
     title: "Kodi | Open Source Home Theater Software",
     description:
-      "Kodi is a free media player that is designed to look great on your big screen TV but is just as home on a small screen.",
+      "Kodi is a free media player that is designed to look great on your big screen TV but is just as at home on a small screen.",
     siteUrl: url,
     author: "Team Kodi",
   },
