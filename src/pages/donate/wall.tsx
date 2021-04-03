8f199f6096b2ddb9646a2e58f9da6d899bf22f09
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-04-03 12:08:04 -1000

    updated donor wall to use Featured Card

--- src/pages/donate/wall.tsx
+++ src/pages/donate/wall.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { DefaultLayout } from "../../components/Layout";
-import DonorList from "../../components/DonorList";
+import { FeaturedCard } from "../../components/Card";
+import { UserCircleOutline } from "heroicons-react";
 
 export default function Page({ data, pageContext, location }) {
   let frontmatter = { title: "Recent Donors", breadcrumbs: "Donors | Wall" };
@@ -9,7 +10,30 @@ export default function Page({ data, pageContext, location }) {
     <>
       <DefaultLayout frontmatter={frontmatter}>
         Here are some of our recent donors. Thanks for your support!
-        <DonorList items={data.Donors.edges} />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {data.Donors.edges.map((donor, index) => (
+            <FeaturedCard
+              title={
+                donor.node.publicName == "" ? "Anonymous" : donor.node.publicName
+              }
+              icon={
+                <UserCircleOutline className="flex-shrink-0 h-6 w-6 text-white" />
+              }
+            >
+              {(donor.node.currency == "usd" ||
+              donor.node.currency == "cad" ||
+              donor.node.currency == "aud"
+                ? "$"
+                : "") +
+                (donor.node.currency == "eur" ? "€" : "") +
+                (donor.node.currency == "gbp" ? "£" : "") +
+                (donor.node.currency == "jpy" ? "¥" : "") +
+                donor.node.amount.toString() +
+                " " +
+                donor.node.currency.toUpperCase()}
+            </FeaturedCard>
+          ))}
+        </div>
       </DefaultLayout>
     </>
   );
