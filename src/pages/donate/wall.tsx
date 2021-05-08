ddf6970e3db19ac2f59301d2eaa0560988270133
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-05-08 05:51:26 -1000

    fix for donorwall source fail on AWS pagination call

--- src/pages/donate/wall.tsx
+++ src/pages/donate/wall.tsx
@@ -12,24 +12,26 @@ export default function Page({ data, pageContext, location }) {
         Here are some of our recent donors. Thanks for your support!
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {data.Donors.nodes.map((donor, index) => (
-            <FeaturedCard
-              title={donor.publicName == "" ? "Anonymous" : donor.publicName}
-              icon={
-                <UserCircleIcon className="flex-shrink-0 h-6 w-6 text-gray-50" />
-              }
-            >
-              {(donor.currency == "usd" ||
-              donor.currency == "cad" ||
-              donor.currency == "aud"
-                ? "$"
-                : "") +
-                (donor.currency == "eur" ? "€" : "") +
-                (donor.currency == "gbp" ? "£" : "") +
-                (donor.currency == "jpy" ? "¥" : "") +
-                donor.amount.toString() +
-                " " +
-                donor.currency.toUpperCase()}
-            </FeaturedCard>
+            <div key={index}>
+              <FeaturedCard
+                title={donor.publicName == "" ? "Anonymous" : donor.publicName}
+                icon={
+                  <UserCircleIcon className="flex-shrink-0 h-6 w-6 text-gray-50" />
+                }
+              >
+                {(donor.currency == "usd" ||
+                donor.currency == "cad" ||
+                donor.currency == "aud"
+                  ? "$"
+                  : "") +
+                  (donor.currency == "eur" ? "€" : "") +
+                  (donor.currency == "gbp" ? "£" : "") +
+                  (donor.currency == "jpy" ? "¥" : "") +
+                  donor.amount.toString() +
+                  " " +
+                  donor.currency.toUpperCase()}
+              </FeaturedCard>
+            </div>
           ))}
         </div>
       </DefaultLayout>
