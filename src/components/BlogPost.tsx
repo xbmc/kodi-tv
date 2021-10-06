d96751e7a863db36ff30781569ed5aed9fc0f034
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2021-10-06 15:59:35 -0400

    Revert "Merge pull request #212 from razzeee/blog-header"
    This reverts commit 679fc812feaf30c682c1b80e89b44a880ccc5103, reversing
    changes made to 4f2b93db9f167c86b989b73473b75403a1a8bfa5.

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -38,7 +38,7 @@ export default function BlogPost(props: { preview?: any; blogPost?: any }) {
           <div className="relative shadow-xl rounded-2xl overflow-hidden">
             <div className="absolute inset-0">
               <img
-                className="h-full w-full object-fill"
+                className="h-full w-full object-cover"
                 src={blogPost.frontmatter.featured_image.src}
                 title={blogPost.frontmatter.featured_image.title}
                 alt={blogPost.frontmatter.featured_image.alt}
