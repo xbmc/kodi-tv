f123f75b169f2e9e9356d6ba7b2f453505ce8c0c
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2022-06-25 15:45:40 -0400

    Revert "package updates"
    This reverts commit 3a64ca31bb5f9199e2b7fbb9b3bf87f3f777d104.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
