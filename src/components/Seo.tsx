0e2164ccb150db8a1b875de1c17aa989dcec56e2
Kolja Lampe <razzeee@gmail.com>
2026-03-27 21:21:52 +0000

    refactor: add keys to SEO component meta tags for improved performance

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -26,20 +26,25 @@ export default function SEO({ frontmatter = {} }: SeoProps) {
   }
   return (
     <>
-      <html lang="en" />
-      <title>{breadcrumbs}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={shortcrumbs} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={shortcrumbs} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
-      <meta name="twitter:creator" content="@KodiTV" />
-      <meta name="twitter:site" content="@KodiTV" />
+      <html key="html-lang" lang="en" />
+      <title key="title">{breadcrumbs}</title>
+      <meta key="description" name="description" content={description} />
+      <meta key="og:title" property="og:title" content={shortcrumbs} />
+      <meta key="og:description" property="og:description" content={description} />
+      <meta key="og:image" property="og:image" content={image} />
+      <meta key="og:type" property="og:type" content="website" />
+      <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
+      <meta key="twitter:title" name="twitter:title" content={shortcrumbs} />
+      <meta
+        key="twitter:description"
+        name="twitter:description"
+        content={description}
+      />
+      <meta key="twitter:image" name="twitter:image" content={image} />
+      <meta key="twitter:creator" name="twitter:creator" content="@KodiTV" />
+      <meta key="twitter:site" name="twitter:site" content="@KodiTV" />
       <script
+        key="goatcounter"
         data-goatcounter="https://koditv.goatcounter.com/count"
         async
         src="//gc.zgo.at/count.js"
