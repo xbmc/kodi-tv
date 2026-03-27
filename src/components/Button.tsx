fd10dc46908dc74ba76f48e187bc548acd18c79c
Kolja Lampe <razzeee@gmail.com>
2026-03-27 12:48:34 +0000

    refactor: convert Button to functional component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,90 +12,89 @@ interface Props {
   children: React.ReactNode;
 }
 
-class Button extends React.Component<Props> {
-  render() {
-    const buttonStyle =
-      this.props.variant === "secondary"
-        ? `
-            flex
-            items-center
-            justify-center
-            px-8
-            py-3
-            mr-2
-            border
-            border-transparent
-            text-base
-            font-medium
-            rounded-md
-            shadow-sm
-            text-kodi-darker
-            bg-gray-50
-            transition
-            duration-500
-            ease
-            select-none
-            hover:bg-gray-200
-            `
-        : `
-            flex
-            items-center
-            justify-center
-            px-8
-            py-3
-            mr-2
-            border
-            border-transparent
-            text-base
-            font-medium
-            rounded-md
-            shadow-sm
-            text-gray-50
-            bg-kodi-darker
-            transition
-            duration-500
-            ease
-            select-none
-            hover:bg-kodi
-            focus:outline-none
-            focus:shadow-outline`;
+function Button({
+  href,
+  disabled,
+  target,
+  variant,
+  rel,
+  buttonType,
+  onClick,
+  children,
+}: Props) {
+  const buttonStyle =
+    variant === "secondary"
+      ? `
+          flex
+          items-center
+          justify-center
+          px-8
+          py-3
+          mr-2
+          border
+          border-transparent
+          text-base
+          font-medium
+          rounded-md
+          shadow-sm
+          text-kodi-darker
+          bg-gray-50
+          transition
+          duration-500
+          ease
+          select-none
+          hover:bg-gray-200
+          `
+      : `
+          flex
+          items-center
+          justify-center
+          px-8
+          py-3
+          mr-2
+          border
+          border-transparent
+          text-base
+          font-medium
+          rounded-md
+          shadow-sm
+          text-gray-50
+          bg-kodi-darker
+          transition
+          duration-500
+          ease
+          select-none
+          hover:bg-kodi
+          focus:outline-none
+          focus:shadow-outline`;
 
-    if (this.props.buttonType === "button") {
-      return (
-        <button
-          disabled={this.props.disabled}
-          onClick={this.props.onClick}
-          type="button"
-          className={buttonStyle}
-        >
-          <span>{this.props.children}</span>
+  if (buttonType === "button") {
+    return (
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        type="button"
+        className={buttonStyle}
+      >
+        <span>{children}</span>
+      </button>
+    );
+  } else if (buttonType === "internal") {
+    return (
+      <Link to={href} target={target} rel={rel}>
+        <button disabled={disabled} type="button" className={buttonStyle}>
+          <span>{children}</span>
         </button>
-      );
-    } else if (this.props.buttonType === "internal") {
-      return (
-        <Link to={this.props.href} target={this.props.target} rel={this.props.rel}>
-          <button
-            disabled={this.props.disabled}
-            type="button"
-            className={buttonStyle}
-          >
-            <span>{this.props.children}</span>
-          </button>
-        </Link>
-      );
-    } else {
-      return (
-        <a href={this.props.href} target={this.props.target} rel={this.props.rel}>
-          <button
-            disabled={this.props.disabled}
-            type="button"
-            className={buttonStyle}
-          >
-            <span>{this.props.children}</span>
-          </button>
-        </a>
-      );
-    }
+      </Link>
+    );
+  } else {
+    return (
+      <a href={href} target={target} rel={rel}>
+        <button disabled={disabled} type="button" className={buttonStyle}>
+          <span>{children}</span>
+        </button>
+      </a>
+    );
   }
 }
 
