47dc61760185049a55f609c6ac064fcf0f955ee0
Kolja Lampe <razzeee@gmail.com>
2026-03-27 13:14:13 +0000

    refactor: convert Tabs and Tab to functional components

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 interface Props {
   activeTab: string;
@@ -6,32 +6,19 @@ interface Props {
   onClick: (label: string) => void;
 }
 
-class Tab extends Component<Props> {
+function Tab({ activeTab, label, onClick }: Props) {
+  let className =
+    "text-gray-600 pt-4 pb-2 px-6 block hover:text-blue-500 focus:outline-none";
 
-  onClick = () => {
-    const { label, onClick } = this.props;
-    onClick(label);
-  };
-
-  render() {
-    const {
-      onClick,
-      props: { activeTab, label },
-    } = this;
-
-    let className =
-      "text-gray-600 pt-4 pb-2 px-6 block hover:text-blue-500 focus:outline-none";
-
-    if (activeTab === label) {
-      className += " text-blue-500 border-b-2 font-medium border-blue-500";
-    }
-
-    return (
-      <button onClick={onClick} className={className}>
-        {label}
-      </button>
-    );
+  if (activeTab === label) {
+    className += " text-blue-500 border-b-2 font-medium border-blue-500";
   }
+
+  return (
+    <button onClick={() => onClick(label)} className={className}>
+      {label}
+    </button>
+  );
 }
 
 export default Tab;
