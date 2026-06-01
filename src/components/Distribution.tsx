6a2dce772e1b9d5f1bda7c14281ec2cfbdc561fd
keith <keith@MacBook-Pro.local>
2026-06-01 08:52:51 -0700

    chore: remove download ad blocks

--- src/components/Distribution.tsx
+++ src/components/Distribution.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { DefaultLayout } from "./Layout";
 import ReleasesTabs from "./ReleasesTabs";
-import { AdUnit } from "./AdUnit";
 
 function Distribution(props) {
   const dist = props.dist;
@@ -27,22 +26,13 @@ function Distribution(props) {
             decoding="async"
           />
         </div>
-        <div className="col-span-1 md:col-span-10 2xl:col-span-7">
+        <div className="col-span-1 md:col-span-10">
           <span className="prose prose-blue">
             If you need extra help, checkout our{" "}
             <a href={dist.howto}>"How To" Guide</a> for {dist.name}.
           </span>
           <ReleasesTabs platform={dist.name} releases={dist.releases} />
-          <div className="mt-8 2xl:hidden">
-            <AdUnit minHeightClass="min-h-72" />
-          </div>
         </div>
-        <aside className="hidden 2xl:col-span-3 2xl:block">
-          <AdUnit minHeightClass="min-h-72" className="sticky top-6" />
-        </aside>
-      </div>
-      <div className="mt-8">
-        <AdUnit minHeightClass="min-h-28" />
       </div>
     </DefaultLayout>
   );
